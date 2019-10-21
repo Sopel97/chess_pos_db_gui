f15f72d40dd19329b9fb9091cb83a3f9d833191f
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2019-10-21 15:42:00 +0200

    Use new tcp api.

--- DatabaseWrapper.cs
+++ DatabaseWrapper.cs
@@ -20,7 +20,7 @@ namespace chess_pos_db_gui
         {
             process = new Process();
             process.StartInfo.FileName = "chess_pos_db.exe";
-            process.StartInfo.Arguments = "tcp \"" + path + "\" " + port;
+            process.StartInfo.Arguments = "tcp " + port;
 
             // TODO: Setting this to true makes the program hang after a few queries
             //       Find a fix as it will be needed for reporting progress to the user.
@@ -36,6 +36,17 @@ namespace chess_pos_db_gui
                 try
                 {
                     client = new TcpClient(address, port);
+
+                    var bytes = System.Text.Encoding.UTF8.GetBytes("{\"command\":\"open\", \"database_path\":\"" + path + "\"}");
+                    var stream = client.GetStream();
+                    stream.Write(bytes, 0, bytes.Length);
+
+                    var response = Read(stream);
+                    if (JsonValue.Parse(response).ContainsKey("error"))
+                    {
+                        throw new InvalidDataException("Cannot open database.");
+                    }
+
                     break;
                 }
                 catch
@@ -44,6 +55,7 @@ namespace chess_pos_db_gui
                 }
             }
         }
+
         private QueryResponse ExecuteQuery(string query)
         {
             var stream = client.GetStream();
@@ -51,6 +63,36 @@ namespace chess_pos_db_gui
             var bytes = System.Text.Encoding.UTF8.GetBytes(query);
             stream.Write(bytes, 0, bytes.Length);
 
+            var responseJson = JsonValue.Parse(Read(stream));
+            if (responseJson.ContainsKey("error"))
+            {
+                throw new InvalidDataException(responseJson["error"].ToString());
+            }
+
+            return QueryResponse.FromJson(responseJson);
+        }
+
+        public QueryResponse Query(string fen)
+        {
+            string query = "{\"command\":\"query\", \"query\":{\"continuations\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false},\"levels\":[\"human\",\"engine\",\"server\"],\"positions\":[{\"fen\":\"" + fen + "\"}],\"results\":[\"win\",\"loss\",\"draw\"],\"token\":\"toktok\",\"transpositions\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false}}}";
+            return ExecuteQuery(query);   
+        }
+        public QueryResponse Query(string fen, string san)
+        {
+            System.Diagnostics.Debug.WriteLine(san);
+            string query = "{\"command\":\"query\", \"query\":{\"continuations\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false},\"levels\":[\"human\",\"engine\",\"server\"],\"positions\":[{\"fen\":\"" + fen + "\", \"move\":\"" + san + "\"}],\"results\":[\"win\",\"loss\",\"draw\"],\"token\":\"toktok\",\"transpositions\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false}}}";
+            return ExecuteQuery(query);
+        }
+
+        public void Close()
+        {
+            client.Close();
+            process.StandardInput.WriteLine("exit");
+            process.Close();
+        }
+
+        private string Read(NetworkStream stream)
+        {
             using (var writer = new MemoryStream())
             {
                 byte[] readBuffer = new byte[client.ReceiveBufferSize];
@@ -66,33 +108,8 @@ namespace chess_pos_db_gui
                 while (stream.DataAvailable);
 
                 var response = Encoding.UTF8.GetString(writer.ToArray());
-                var responseJson = JsonValue.Parse(response);
-                if (responseJson.ContainsKey("error"))
-                {
-                    throw new InvalidDataException(responseJson["error"].ToString());
-                }
-
-                return QueryResponse.FromJson(responseJson);
+                return response;
             }
-        }
-
-        public QueryResponse Query(string fen)
-        {
-            string query = "{\"continuations\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false},\"levels\":[\"human\",\"engine\",\"server\"],\"positions\":[{\"fen\":\"" + fen + "\"}],\"results\":[\"win\",\"loss\",\"draw\"],\"token\":\"toktok\",\"transpositions\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false}}";
-            return ExecuteQuery(query);   
-        }
-        public QueryResponse Query(string fen, string san)
-        {
-            System.Diagnostics.Debug.WriteLine(san);
-            string query = "{\"continuations\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false},\"levels\":[\"human\",\"engine\",\"server\"],\"positions\":[{\"fen\":\"" + fen + "\", \"move\":\"" + san + "\"}],\"results\":[\"win\",\"loss\",\"draw\"],\"token\":\"toktok\",\"transpositions\":{\"fetch_children\":true,\"fetch_first_game\":true,\"fetch_first_game_for_each_child\":true,\"fetch_last_game\":false,\"fetch_last_game_for_each_child\":false}}";
-            return ExecuteQuery(query);
-        }
-
-        public void Close()
-        {
-            client.Close();
-            process.StandardInput.WriteLine("exit");
-            process.Close();
         }
     }
 }
