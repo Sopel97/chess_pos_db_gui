ccd2148e5da08339ddb09f6c8f4b5c94dbda679d
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-03-28 18:46:45 +0100

    Add elo calculator

---
+++ EloCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_pos_db_gui
+{
+    class EloCalculator
+    {
+        public static double GetExpectedPerformance(double eloDiff)
+        {
+            return 1.0 / (1.0 + Math.Pow(10.0, -eloDiff / 400.0));
+        }
+
+        public static double GetExpectedPerformance(double whiteElo, double blackElo)
+        {
+            return GetExpectedPerformance(whiteElo - blackElo);
+        }
+    }
+}
