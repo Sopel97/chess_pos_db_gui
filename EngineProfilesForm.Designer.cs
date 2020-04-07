dfd43ae67365325e5e33782714c2486f5ab8e6f7
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-04-07 18:13:05 +0200

    Resizable engine profiles gui.

--- EngineProfilesForm.Designer.cs
+++ EngineProfilesForm.Designer.cs
@@ -36,17 +36,21 @@
             // 
             // profilesListBox
             // 
+            this.profilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.profilesListBox.FormattingEnabled = true;
             this.profilesListBox.Location = new System.Drawing.Point(12, 12);
             this.profilesListBox.Name = "profilesListBox";
-            this.profilesListBox.Size = new System.Drawing.Size(234, 186);
+            this.profilesListBox.Size = new System.Drawing.Size(367, 212);
             this.profilesListBox.TabIndex = 0;
             // 
             // confirmButton
             // 
-            this.confirmButton.Location = new System.Drawing.Point(172, 204);
+            this.confirmButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.confirmButton.Location = new System.Drawing.Point(277, 232);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(74, 23);
+            this.confirmButton.Size = new System.Drawing.Size(102, 23);
             this.confirmButton.TabIndex = 1;
             this.confirmButton.Text = "Select";
             this.confirmButton.UseVisualStyleBackColor = true;
@@ -54,7 +58,8 @@
             // 
             // addProfileButton
             // 
-            this.addProfileButton.Location = new System.Drawing.Point(12, 204);
+            this.addProfileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.addProfileButton.Location = new System.Drawing.Point(12, 232);
             this.addProfileButton.Name = "addProfileButton";
             this.addProfileButton.Size = new System.Drawing.Size(74, 23);
             this.addProfileButton.TabIndex = 2;
@@ -64,7 +69,8 @@
             // 
             // removeProfileButton
             // 
-            this.removeProfileButton.Location = new System.Drawing.Point(92, 204);
+            this.removeProfileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.removeProfileButton.Location = new System.Drawing.Point(92, 232);
             this.removeProfileButton.Name = "removeProfileButton";
             this.removeProfileButton.Size = new System.Drawing.Size(74, 23);
             this.removeProfileButton.TabIndex = 3;
@@ -76,13 +82,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 453);
+            this.ClientSize = new System.Drawing.Size(384, 262);
             this.Controls.Add(this.removeProfileButton);
             this.Controls.Add(this.addProfileButton);
             this.Controls.Add(this.confirmButton);
             this.Controls.Add(this.profilesListBox);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "EngineProfilesForm";
-            this.Text = "EngineProfilesForm";
+            this.Text = "Engine profiles";
             this.ResumeLayout(false);
 
         }
