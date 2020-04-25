6dca41078fcaf98729cc78f1a5ce6e587ac6cbc5
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-04-25 23:41:59 +0200

    Theme representation.

---
+++ src/app/board/BoardTheme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_pos_db_gui.src.app.board
+{
+    class BoardTheme
+    {
+        public Image WhiteSquare { get; private set; }
+        public Image BlackSquare { get; private set; }
+
+        public BoardTheme(string path)
+        {
+            try
+            {
+                WhiteSquare = Image.FromFile(path + "/white_square.png");
+                BlackSquare = Image.FromFile(path + "/black_square.png");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                SetDefaultImages();
+            }
+        }
+
+        private void SetDefaultImages()
+        {
+            const int size = 80;
+
+            var w = new Bitmap(size, size);
+            Graphics.FromImage(w).Clear(Color.FromArgb(0xf0d9b5));
+
+            var b = new Bitmap(size, size);
+            Graphics.FromImage(w).Clear(Color.FromArgb(0xb58863));
+
+            WhiteSquare = w;
+            BlackSquare = b;
+        }
+    }
+}
---
+++ src/app/board/PieceTheme.cs
@@ -0,0 +1,38 @@
+﻿using ChessDotNet;
+using ChessDotNet.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_pos_db_gui.src.app.board
+{
+    class PieceTheme
+    {
+        private Dictionary<Piece, Image> PieceImages { get; set; }
+
+        public PieceTheme(string path)
+        {
+            PieceImages.Add(new Pawn(Player.White), Image.FromFile(path + "/white_pawn.png"));
+            PieceImages.Add(new Knight(Player.White), Image.FromFile(path + "/white_knight.png"));
+            PieceImages.Add(new Bishop(Player.White), Image.FromFile(path + "/white_bishop.png"));
+            PieceImages.Add(new Rook(Player.White), Image.FromFile(path + "/white_rook.png"));
+            PieceImages.Add(new Queen(Player.White), Image.FromFile(path + "/white_queen.png"));
+            PieceImages.Add(new King(Player.White), Image.FromFile(path + "/white_king.png"));
+
+            PieceImages.Add(new Pawn(Player.Black), Image.FromFile(path + "/black_pawn.png"));
+            PieceImages.Add(new Knight(Player.Black), Image.FromFile(path + "/black_knight.png"));
+            PieceImages.Add(new Bishop(Player.Black), Image.FromFile(path + "/black_bishop.png"));
+            PieceImages.Add(new Rook(Player.Black), Image.FromFile(path + "/black_rook.png"));
+            PieceImages.Add(new Queen(Player.Black), Image.FromFile(path + "/black_queen.png"));
+            PieceImages.Add(new King(Player.Black), Image.FromFile(path + "/black_king.png"));
+        }
+
+        public Image GetImageForPiece(Piece piece)
+        {
+            return PieceImages[piece];
+        }
+    }
+}
