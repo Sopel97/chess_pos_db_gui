d55846321f7bd64e877d1699ff542d13f89e52cc
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-05-15 21:49:37 +0200

    some preliminary grouping stuff.

--- src/app/forms/DatabaseMergeForm.Designer.cs
+++ src/app/forms/DatabaseMergeForm.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.addButton = new System.Windows.Forms.Button();
+            this.entriesView = new System.Windows.Forms.ListView();
+            this.makeGroupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(338, 12);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // entriesView
+            // 
+            this.entriesView.FullRowSelect = true;
+            this.entriesView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.entriesView.Location = new System.Drawing.Point(12, 12);
+            this.entriesView.Name = "entriesView";
+            this.entriesView.Size = new System.Drawing.Size(295, 426);
+            this.entriesView.TabIndex = 2;
+            this.entriesView.UseCompatibleStateImageBehavior = false;
+            this.entriesView.View = System.Windows.Forms.View.Details;
+            this.entriesView.VirtualMode = true;
+            this.entriesView.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.entriesView_RetrieveVirtualItem);
+            this.entriesView.SelectedIndexChanged += new System.EventHandler(this.entriesView_SelectedIndexChanged);
+            this.entriesView.VirtualItemsSelectionRangeChanged += new System.Windows.Forms.ListViewVirtualItemsSelectionRangeChangedEventHandler(this.entriesView_VirtualItemsSelectionRangeChanged);
+            // 
+            // makeGroupButton
+            // 
+            this.makeGroupButton.Location = new System.Drawing.Point(338, 41);
+            this.makeGroupButton.Name = "makeGroupButton";
+            this.makeGroupButton.Size = new System.Drawing.Size(75, 23);
+            this.makeGroupButton.TabIndex = 3;
+            this.makeGroupButton.Text = "makeGroup";
+            this.makeGroupButton.UseVisualStyleBackColor = true;
+            this.makeGroupButton.Click += new System.EventHandler(this.makeGroupButton_Click);
+            // 
+            // DatabaseMergeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.makeGroupButton);
+            this.Controls.Add(this.entriesView);
+            this.Controls.Add(this.addButton);
+            this.Name = "DatabaseMergeForm";
             this.Text = "DatabaseMergeForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListView entriesView;
+        private System.Windows.Forms.Button makeGroupButton;
     }
 }
--- src/app/forms/DatabaseMergeForm.cs
+++ src/app/forms/DatabaseMergeForm.cs
@@ -10,11 +10,214 @@ using System.Windows.Forms;
 
 namespace chess_pos_db_gui.src.app.forms
 {
+    // TODO: support only consecutive merging
+    //       Currently there is no such databases, but there may in the future.
+    //       It's harder to implement and actually define.
     public partial class DatabaseMergeForm : Form
     {
+        int i = 0;
+        private List<Entry> Entries { get; set; }
+
         public DatabaseMergeForm()
         {
             InitializeComponent();
+
+            Entries = new List<Entry>();
+
+            entriesView.Columns.Add("one");
+            entriesView.Columns.Add("two");
+
+            entriesView.VirtualListSize = 0;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            Entries.Add(Entry.MakeEntry(i.ToString(), (ulong)i));
+            i += 1;
+
+            entriesView.VirtualListSize = Entries.Count;
+        }
+
+        private void entriesView_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("dragdrop");
+        }
+
+        private void entriesView_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            var entry = Entries[e.ItemIndex];
+            e.Item = new ListViewItem(new string[] { entry.GetFormattedName(), entry.GetFormattedSize() });
+        }
+
+        private void makeGroupButton_Click(object sender, EventArgs e)
+        {
+            var selectedIndices = entriesView.SelectedIndices;
+            if (selectedIndices.Count < 1)
+            {
+                return;
+            }
+
+            int insertIndex = Entries.Count;
+            Entry groupHeader = Entry.MakeGroupHeader();
+            List<Entry> groupedEntries = new List<Entry>();
+            foreach (int index in selectedIndices)
+            {
+                var entry = Entries[index];
+                if (entry.IsGroupHeader)
+                {
+                    continue;
+                }
+
+                // We look for the minimal index
+                insertIndex = Math.Min(index, insertIndex);
+
+                entry.SetParent(groupHeader);
+                groupedEntries.Add(entry);
+            }
+
+            Entries.RemoveAll(entry => entry.Parent == groupHeader);
+
+            // We may have stolen something from a different group. 
+            // We have to find the end of it to insert the new group.
+            if (insertIndex < Entries.Count && Entries[insertIndex].Parent != null)
+            {
+                var baseParent = Entries[insertIndex].Parent;
+                while (insertIndex < Entries.Count && Entries[insertIndex].Parent == baseParent)
+                    ++insertIndex;
+            }
+
+            Entries.InsertRange(insertIndex, groupedEntries);
+            Entries.Insert(insertIndex, groupHeader);
+
+            RemoveEmptyGroups(Entries);
+
+            entriesView.VirtualListSize = Entries.Count;
+
+            entriesView.SelectedIndices.Clear();
+
+            entriesView.Refresh();
+        }
+
+        private void RemoveEmptyGroups(List<Entry> entries)
+        {
+            HashSet<Entry> usedGroups = new HashSet<Entry>();
+            foreach (var entry in entries)
+            {
+                if (entry.IsGroupHeader || entry.Parent == null)
+                {
+                    continue;
+                }
+
+                usedGroups.Add(entry.Parent);
+            }
+
+            entries.RemoveAll(e => e.IsGroupHeader && !usedGroups.Contains(e));
+        }
+
+        private void entriesView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RemoveSelectionOnGroups();
+        }
+
+        private void entriesView_VirtualItemsSelectionRangeChanged(object sender, ListViewVirtualItemsSelectionRangeChangedEventArgs e)
+        {
+            RemoveSelectionOnGroups();
+        }
+
+        private void RemoveSelectionOnGroups()
+        {
+            foreach(int index in entriesView.SelectedIndices)
+            {
+                if (Entries[index].IsGroupHeader)
+                {
+                    entriesView.SelectedIndices.Remove(index);
+                    break;
+                }
+            }
         }
     }
+
+    class Entry
+    {
+        private static readonly string indent = "    ";
+        private static ulong nextUniqueId = 0;
+
+        private ulong UniqueId { get; set; }
+
+        public bool IsGroupHeader { get; private set; }
+        public Entry Parent { get; private set; } 
+
+        public string Name { get; private set; }
+        public ulong Size { get; private set; }
+
+        public static Entry MakeGroupHeader()
+        {
+            return new Entry
+            {
+                UniqueId = nextUniqueId++,
+                IsGroupHeader = true,
+                Parent = null,
+                Name = "GROUP",
+                Size = 0
+            };
+        }
+
+        public static Entry MakeEntry(string name, ulong size)
+        {
+            return new Entry
+            {
+                UniqueId = nextUniqueId++,
+                IsGroupHeader = false,
+                Parent = null,
+                Name = name,
+                Size = size
+            };
+        }
+
+        private Entry()
+        {
+
+        }
+
+        public string GetFormattedName()
+        {
+            if (Parent != null)
+            {
+                return indent + Name;
+            }
+            else
+            {
+                return Name;
+            }
+        }
+
+        public string GetFormattedSize()
+        {
+            if (Parent != null)
+            {
+                return indent + Size.ToString();
+            }
+            else
+            {
+                return Size.ToString();
+            }
+        }
+
+        public void SetParent(Entry e)
+        {
+            Parent = e;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Entry entry &&
+                   UniqueId == entry.UniqueId;
+        }
+
+        public override int GetHashCode()
+        {
+            return -401120461 + UniqueId.GetHashCode();
+        }
+    }
+
 }
