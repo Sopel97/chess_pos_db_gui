bde7366e3cdfebb5f06a5437180aa7dd325a6315
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-05-16 00:12:15 +0200

    remove unneeded stuff

--- src/app/forms/DatabaseMergeForm.cs
+++ src/app/forms/DatabaseMergeForm.cs
@@ -1,16 +1,9 @@
 ﻿using chess_pos_db_gui.src.util;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 namespace chess_pos_db_gui.src.app.forms
 {
@@ -40,11 +33,6 @@ namespace chess_pos_db_gui.src.app.forms
             entryGroupsView.VirtualListSize = 0;
             WinFormsControlUtil.MakeDoubleBuffered(entryGroupsView);
 
-        }
-
-        private List<Element> GetElementsByIndices(List<int> indices)
-        {
-            return null;
         }
 
         private void addButton_Click(object sender, EventArgs e)
